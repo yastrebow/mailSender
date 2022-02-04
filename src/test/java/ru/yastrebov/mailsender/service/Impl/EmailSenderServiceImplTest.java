05fcb9155eb345426da5b260474225cfb03946e0
yastrebow <yastrebow@gmail.com>
2022-02-04 12:31:56 +0300

    Create verify-test and clear the code

--- src/test/java/ru/yastrebov/mailsender/service/Impl/EmailSenderServiceImplTest.java
+++ src/test/java/ru/yastrebov/mailsender/service/Impl/EmailSenderServiceImplTest.java
@@ -2,20 +2,12 @@ package ru.yastrebov.mailsender.service.Impl;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 class EmailSenderServiceImplTest {
